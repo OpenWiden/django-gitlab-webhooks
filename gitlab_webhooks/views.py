d4cac5347c085266da6905c0ed1d505083be3fda
Alexandr <stefanitsky.mozdor@gmail.com>
2020-07-19 15:33:36 +0300

    :sparkles: add webhook handle view

--- gitlab_webhooks/views.py
+++ gitlab_webhooks/views.py
@@ -1,19 +1,66 @@
-from django import views
+import json
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.dispatch import Signal
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.http import HttpRequest, JsonResponse
-from gitlab_webhooks import __version__
+from django.views.decorators.csrf import csrf_exempt
+
+from . import constants, signals
 
 
-class HealthCheckView(views.View):
-    def get(self, request: HttpRequest):
+@method_decorator(csrf_exempt, "dispatch")
+class WebhookView(View):
+    def get_secret(self) -> str:
+        """
+        Returns webhook's secret key.
+        """
+        secret = settings.DJANGO_GITLAB_WEBHOOKS.get("SECRET")
+        if not secret:
+            raise ImproperlyConfigured("SECRET key is not specified!")
+        else:
+            return secret
+
+    @classmethod
+    def event_is_allowed(cls, event: str) -> bool:
+        if event in settings.DJANGO_GITLAB_WEBHOOKS["ALLOWED_EVENTS"]:
+            return True
+        else:
+            return False
+
+    @classmethod
+    def get_signal(cls, event: str) -> Signal:
+        formatted_event_name = event.lower().replace(" hook", "").replace(" ", "_")
+        return getattr(signals, formatted_event_name)
+
+    def post(self, request: HttpRequest, **kwargs) -> JsonResponse:
+        # Validate webhook secret
+        if request.META.get(constants.TOKEN_HEADER) != self.get_secret():
+            return JsonResponse(
+                {"detail": constants.INVALID_HTTP_X_GITLAB_TOKEN},
+                status=400,
+            )
+
+        # Check event header
+        event = request.META.get(constants.EVENT_HEADER)
+        if event is None:
+            return JsonResponse(
+                {"detail": constants.EVENT_HEADER_IS_MISSING},
+                status=400,
+            )
+
+        # Validate that event is allowed
+        event_is_allowed = self.event_is_allowed(event)
+        if event_is_allowed is False:
+            return JsonResponse(
+                {"detail": constants.EVENT_IS_NOT_ALLOWED.format(event=event)},
+                status=400,
+            )
+
+        # Send signal on success event
+        signal = self.get_signal(event)
+        signal.send(__class__, payload=json.loads(request.body))
+
         return JsonResponse({"detail": "ok"})
-
-
-health_check_view = HealthCheckView.as_view()
-
-
-class AppVersionView(views.View):
-    def get(self, request: HttpRequest):
-        return JsonResponse({"detail": __version__})
-
-
-app_version_view = AppVersionView.as_view()
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,21 +1,65 @@
 import json
 
 import pytest
+from django.core.exceptions import ImproperlyConfigured
 from django.test import RequestFactory
-from gitlab_webhooks import __version__, views
+from gitlab_webhooks.views import WebhookView
+from gitlab_webhooks import constants
 
 
-def test_health_check_view(rf: RequestFactory):
-    request = rf.get("/fake-url/")
-    response = views.HealthCheckView().get(request)
+class TestWebhookView:
+    @pytest.mark.parametrize("event", constants.Events.values())
+    def test_success(self, rf: RequestFactory, settings, event: str):
+        settings.DJANGO_GITLAB_WEBHOOKS = {
+            "SECRET": "fake_token",
+            "ALLOWED_EVENTS": [event],
+        }
+        headers = {
+            constants.TOKEN_HEADER: "fake_token",
+            constants.EVENT_HEADER: event,
+        }
+        request = rf.post("/fake-url/", **headers)
+        request._body = '{"test": "ok"}'.encode()
+        response = WebhookView().post(request)
 
-    assert response.status_code == 200
-    assert json.loads(response.content) == {"detail": "ok"}
+        assert response.status_code == 200
+        assert json.loads(response.content) == {"detail": "ok"}
 
+    def test_get_secret(self, settings):
+        settings.DJANGO_GITLAB_WEBHOOKS = {}
 
-def test_app_version_view(rf: RequestFactory):
-    request = rf.get("/fake-url/")
-    response = views.AppVersionView().get(request)
+        with pytest.raises(ImproperlyConfigured):
+            WebhookView().get_secret()
 
-    assert response.status_code == 200
-    assert json.loads(response.content) == {"detail": __version__}
+    def test_invalid_token(self, rf: RequestFactory, settings):
+        settings.DJANGO_GITLAB_WEBHOOKS = {"SECRET": "fake_token"}
+        headers = {constants.TOKEN_HEADER: "invalid"}
+        request = rf.post("/fake-url/", **headers)
+        response = WebhookView().post(request)
+
+        assert response.status_code == 400
+        assert json.loads(response.content.decode()) == {
+            "detail": constants.INVALID_HTTP_X_GITLAB_TOKEN
+        }
+
+    def test_event_header_is_missing(self, rf: RequestFactory, settings):
+        settings.DJANGO_GITLAB_WEBHOOKS = {"SECRET": "fake_token"}
+        headers = {constants.TOKEN_HEADER: "fake_token"}
+        request = rf.post("/fake-url/", **headers)
+        response = WebhookView().post(request)
+
+        assert response.status_code == 400
+        assert json.loads(response.content.decode()) == {
+            "detail": constants.EVENT_HEADER_IS_MISSING
+        }
+
+    def test_event_is_not_allowed(self, rf: RequestFactory, settings):
+        settings.DJANGO_GITLAB_WEBHOOKS = {"SECRET": "fake_token", "ALLOWED_EVENTS": []}
+        headers = {constants.TOKEN_HEADER: "fake_token", constants.EVENT_HEADER: "test"}
+        request = rf.post("/fake-url/", **headers)
+        response = WebhookView().post(request)
+
+        assert response.status_code == 400
+        assert json.loads(response.content.decode()) == {
+            "detail": constants.EVENT_IS_NOT_ALLOWED.format(event="test")
+        }
