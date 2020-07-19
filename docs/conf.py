7947988a0ce4d1e592e8f0537df8c25151f327b8
Alexandr Stefanitsky-Mozdor <stefanitsky.mozdor@gmail.com>
2020-07-19 15:38:02 +0300

    🔖 Bump version: 0.1.0 → 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = "django-gitlab-webhooks"
 author = "stefanitsky"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
